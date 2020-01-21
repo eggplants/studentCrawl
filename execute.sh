5e1e1fcf2507b82e06d9625ef8448acbae1e801d
haruna <s1811528@s.tsukuba.ac.jp>
2020-01-21 09:07:51 +0900

    some changed

--- execute.sh
+++ execute.sh
@@ -1,6 +1,11 @@
+START=`date +%s`
+
 for i in `ls IDS/s*`;do
-  ruby existcheck.rb < $i 2>/dev/null
+  ruby existcheck.rb < "${i}" 2>/dev/null
 done
+
 mkdir -p LISTS_MD LISTS
 mv LIST_*md LISTS_MD/
 mv LIST_* LISTS/
+END=`date +%s`
+echo "===>>>ALL FINISHED. TIME IS:$((END-START)) sec."
--- existcheck.rb
+++ existcheck.rb
@@ -4,9 +4,8 @@ require "parallel"
 def fetch(ok,u)
   url=URI.parse(u)
   begin
-    if Net::HTTP.new(url.host).get(url.request_uri).code != "404"
-      ok.push(u)
-    end
+    bakcode=Net::HTTP.new(url.host).get(url.request_uri).code
+    ok.push(u) if bakcode=="200"
   rescue;end
 end
 #################################################################
@@ -14,13 +13,14 @@ c,url,out=0,[],[]
 (i=`dd`.split)   .each   { | id       |
 ['http','https'] .each   { | protocol |
 ['htm','html']   .each   { | sort     |
-  url<<"#{protocol}://www.u.tsukuba.ac.jp/~#{id}/index.#{sort}"
+  url<<'%s://www.u.tsukuba.ac.jp/~%s/index.%s'%[protocol,id,sort]
 }}}
 #################################################################
-puts("size = #{url.size}")
-puts(n=i[0][0,3])
-Parallel.map(url,in_threads: 10){|i|c+=1;print(c,"\r");fetch(out,i)}
+puts('ID:%sXXXXX'%(n=i[0][0,3]))
+puts('SIZE:%d'%url.size)
+Parallel.map(url,in_threads: 10){|i|c+=1;print(c,?\r);fetch(out,i)}
+puts('=>Finished!')
 File.open('LIST_%s'%n,'w'){|f|out.each{|r|f.puts(r)}}
 File.open('LIST_%s.md'%n,'w'){|f|
-  out.each{|r|f.puts("[%s](%s)"%[r.scan(/s19\d{5}/)[0],r])}
+  out.each{|r|f.puts('[%s](%s)'%[r.scan(/s19\d{5}/)[0],r])}
 }
