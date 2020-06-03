92be7ff2bde1bb2a9a9772a580964092fe44baf6
eggplants <w10776e8w@yahoo.co.jp>
2020-06-04 05:00:04 +0900

    Add s[^2]. + http

--- existcheck.rb
+++ existcheck.rb
@@ -12,7 +12,7 @@ end
 #################################################################
 c,url,out=0,[],[]
 (i=`dd 2>/dev/null`.split)   .each   { | id       |
-['http', 'https']                     .each   { | protocol |
+['http', 'https']            .each   { | protocol |
 ['htm','html']               .each   { | sort     |
   print id,"\r"
   url<<'%s://www.u.tsukuba.ac.jp/~%s/index.%s'%[protocol,id,sort]
@@ -23,5 +23,5 @@ puts('ID:%sXXXXX'%(n=i[0][0,3]))
 puts('SIZE:%d'%url.size)
 Parallel.map(url,in_threads:30){|i|c+=1;print(c,?\r);fetch(out,i)}
 puts('=>Finished!')
-File.open('./LISTS/%s/LIST_%s'%[nowdate,n],'w'){|f|out.each{|r|f.puts(r)}}
-File.open('./LISTS_MD/%s/LIST_%s.md'%[nowdate,n],'w'){|f|out.each{|r|f.puts('[%s](%s)  '%[r,r])}}
+File.open('./LISTS/%s/LIST_%s'%[nowdate,n],'a'){|f|out.each{|r|f.puts(r)}}
+File.open('./LISTS_MD/%s/LIST_%s.md'%[nowdate,n],'a'){|f|out.each{|r|f.puts('[%s](%s)  '%[r,r])}}
