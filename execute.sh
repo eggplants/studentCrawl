b901ee4244b47f9d98d74afc5ebfd674689ee521
eggplants <w10776e8w@yahoo.co.jp>
2020-06-06 01:15:11 +0900

    Add alllist

--- execute.sh
+++ execute.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-START=$(date +%s)
+START="$(date +%s)"
 today="$(LANG='en_EN' date +%y-%m-%d)"
-
 mkdir -p "LISTS_MD/${today}" "LISTS/${today}"
 
 for i in IDS/*;do
   ruby existcheck.rb < "${i}" # 2>/dev/null
 done
 
-LANG='en_EN' date +%Y-%m-%d_%H:%M:%S>>log
-
-END=$(date +%s)
+cat LISTS/${today}/LIST_s* > "LISTS/${today}/LIST_ALL"
+LANG='en_EN' date +%Y-%m-%d_%H:%M:%S >> log
+END="$(date +%s)"
 echo "===>>>ALL FINISHED. TIME IS:$((END-START)) sec."
